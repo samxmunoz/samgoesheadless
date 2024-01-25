c6c96bd3491810b1406333c7fc60ade1475b210d
Sam Munoz <sam@samanthamunoz.com>
2024-01-25 09:30:10 -0500

    Chapter 00 of SamGoesHeadless

---
+++ app/hello_world/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function hello_world() {
+  return (
+    <div>
+      <div class="hello_sam">Hello Sam, I'm your first Next.js app 👋</div>
+    </div>
+  )
+}
